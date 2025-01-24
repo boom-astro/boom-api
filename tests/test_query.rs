28af6f1d1212bf31b31df62e48e5073bebd4eb61
mbjenson <matthew.jenson@protonmail.com>
2025-01-24 15:27:51 -0600

    created testing branch, test file, and made the /src available as a module

---
+++ tests/test_query.rs
@@ -0,0 +1,11 @@
+use boom_api::api;
+use boom_api::models;
+
+// TODO: start here, and continually expand on unit testing
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_build_options() {
+        
+    }
+}
