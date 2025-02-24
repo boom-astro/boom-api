c7721f542cfdaeaa0e9643f85d6289a1e2d45873
mbjenson <matthew.jenson@protonmail.com>
2025-02-24 17:43:32 -0600

    reimplemented get_object endpoint

--- src/api/alerts.rs
+++ src/api/alerts.rs
@@ -1,118 +1,114 @@
-use crate::models::alert_models;
+use crate::{models::{alert_models, response}, api::util};
 use actix_web::{get, web, HttpResponse};
 use futures::TryStreamExt;
 use mongodb::{
-    bson::{doc, Document},
-    Client, Collection,
+    bson::{doc, Document}, results::SummaryBulkWriteResult, Client, Collection
 };
 
 const DB_NAME: &str = "boom";
 
-// Get an object from a particular catalog by using its objectid
-#[get("/alerts/get_object")]
+// TODO: check notion
+/*
+1. get the most recent detection for the object
+    sort alerts by jd in ascending order
+2. get the image information for that detection
+    get the cutoutScience, cutoutTemplate, and cutoutDifference fields
+3. get metadata for that detection
+    ...
+4. get the crossmatches and prv_candidates for that object from the aux table
+*/
+
+#[get("/alerts/{survey_name}/get_object/{object_id}")]
 pub async fn get_object(
     client: web::Data<Client>,
-    body: web::Json<alert_models::GetObjectBody>,
+    path: web::Path<(String, String)>,
 ) -> HttpResponse {
-    let body = body.clone();
-    let catalog = body.catalog.expect("catalog required for get_object");
-    let collection: Collection<mongodb::bson::Document> = client
+    let (survey_name, object_id) = path.into_inner();
+    let survey_name = survey_name.to_uppercase(); // TEMP to match with "ZTF"
+
+    let alerts_collection: Collection<mongodb::bson::Document> = client
         .database(DB_NAME)
-        .collection(&format!("{}_alerts", catalog));
-    let object_id = body.object_id.expect("objectId required for get_object");
+        .collection(&format!("{}_alerts", survey_name));
 
-    // get brightest alert
-    let find_options = mongodb::options::FindOptions::builder()
-        .sort(doc! {
-            "candidate.magpsf": 1,
-        })
-        .projection(doc! { "_id": 0 })
-        .limit(1)
-        .build();
-
-    let mut brightest_alert_cursor = collection
-        .find(doc! {
-            "objectId": object_id.to_string(),
-        })
-        .with_options(find_options)
-        .await
-        .expect("failed to execute find query");
-    let brightest_alert = match brightest_alert_cursor.try_next().await {
-        Ok(Some(alert)) => alert,
-        Ok(None) => {
-            return HttpResponse::Ok().body(format!("no object found with id {}", object_id));
-        }
-        Err(error) => {
-            return HttpResponse::InternalServerError().body(format!("error: {}", error));
-        }
-    };
-
-    let find_options_alerts = mongodb::options::FindOptions::builder()
-        .projection(doc! {
-            "_id": 0,
-            "cutoutScience": 0,
-            "cutoutTemplate": 0,
-            "curoutDifference": 0,
-        })
+    let aux_collection: Collection<Document> = client
+        .database(DB_NAME)
+        .collection(&format!("{}_alerts_aux", survey_name));
+    
+    // find options for getting most recent alert from alerts collection
+    let find_options_recent = mongodb::options::FindOptions::builder()
         .sort(doc! {
             "candidate.jd": 1,
         })
+        .projection(doc! {
+            "_id": 1,
+            "candidate": 1,
+            "cutoutScience": 1,
+            "cutoutTemplate": 1,
+            "cutoutDifference": 1,
+        })
         .build();
 
-    let cursor_alerts = collection
+    // get the most recent alert for the object
+    let mut alert_cursor = match alerts_collection
         .find(doc! {
-            "objectId": object_id.to_string(),
+            "objectId": object_id.clone(),
         })
-        .with_options(find_options_alerts)
-        .await
-        .unwrap();
-    let alerts = cursor_alerts
-        .try_collect::<Vec<mongodb::bson::Document>>()
-        .await
-        .unwrap();
+        .with_options(find_options_recent)
+        .await {
+            Ok(cursor) => cursor,
+            Err(error) => {
+                return response::internal_error(&format!("error getting documents: {}", error));
+            }
+        };
+    let newest_alert = match alert_cursor
+        .try_next()
+        .await {
+            Ok(Some(alert)) => alert,
+            Ok(None) => {
+                return response::ok(&format!("no object found with id {}", object_id), serde_json::Value::Null);
+            },
+            Err(error) => {
+                return response::internal_error(&format!("error getting documents: {}", error));
+            }
+        };
 
-    let aux_collection_name = format!("{catalog}_alerts_aux");
-    // then fetch the entry for that object in the aux collection
-    let aux_collection: Collection<mongodb::bson::Document> = client
-        .database(DB_NAME)
-        .collection(aux_collection_name.as_str());
-    let aux_entry = aux_collection
+    let find_options_aux = mongodb::options::FindOneOptions::builder()
+        .projection(doc! {
+            "_id": 0,
+            "prv_candidates": 1,
+            "cross_matches": 1,
+        })
+        .build();
+    
+    // get crossmatches and light curve data from aux collection
+    let aux_entry = match aux_collection
         .find_one(doc! {
-            "_id": object_id.to_string(),
+            "_id": object_id.clone(),
         })
-        .await
-        .expect("failed to exexcute find_one query")
-        .expect("no aux entry found");
+        .with_options(find_options_aux)
+        .await {
+            Ok(entry) => {
+                match entry {
+                    Some(doc) => doc,
+                    None => {
+                        return response::ok("no aux entry found", serde_json::Value::Null);
+                    }
+                }
+            },
+            Err(error) => {
+                return response::internal_error(&format!("error getting documents: {}", error));
+            }
+        };
 
-    let mut data = doc! {};
-
-    data.insert("objectId", object_id.to_string());
-    // brightest alert has teh cutoutScience, cutoutTemplate, and cutoutDifference fields
-    // that are byte strings
-    match brightest_alert.get_document("cutoutScience") {
-        Ok(cutout) => {
-            data.insert("cutoutScience", cutout);
-        }
-        Err(error) => {
-            println!("cutoutScience not found: {}", error);
-            // print the type of the cutoutScience field
-            data.insert("cutoutScience", "");
-        }
-    }
-    data.insert("alerts", alerts);
-
-    let prv_candidates = aux_entry.get_array("prv_candidates").unwrap();
-    data.insert("prv_candidates", prv_candidates);
-
-    // crossmatches is a Result<>, so we check if it's Ok or Err
-    match aux_entry.get_document("cross_matches") {
-        Ok(crossmatches) => {
-            data.insert("cross_matches", crossmatches);
-        }
-        Err(_) => {
-            data.insert("cross_matches", doc! {});
-        }
-    }
-
-    HttpResponse::Ok().json(data)
+    let mut data = doc!{};
+    // organize response
+    data.insert("objectId", object_id.clone());
+    data.insert("alert metadata", newest_alert.get_document("candidate").unwrap());
+    data.insert("cutoutScience", newest_alert.get_document("cutoutScience").unwrap());
+    data.insert("cutoutTemplate", newest_alert.get_document("cutoutTemplate").unwrap());
+    data.insert("cutoutDifference", newest_alert.get_document("cutoutDifference").unwrap());
+    data.insert("prv_candidates", aux_entry.get_array("prv_candidates").unwrap());
+    data.insert("cross_matches", aux_entry.get_document("cross_matches").unwrap());
+    
+    return response::ok(&format!("object found with object_id: {}", object_id), serde_json::json!(data));
 }
